59b07083381edaf69c45306846a4511b4643b365
akari0624 <akari0624@hotmail.com>
2017-11-23 17:54:05 +0800

    etup mongoose Promise to global Promise,because mongoose Promise has depecated

--- index.js
+++ index.js
@@ -8,21 +8,28 @@ const cors = require('cors');
 const PORT = 8089;
 const app = express();
 
+// map global Promise - get rid of warning
+mongoose.Promise = global.Promise;
+
 // mongoDB 預設走port 27017          //drink_order_system 就是local mongoDB 裡的database name  有就連,沒有就創出來(超隨性ㄉ) 
-mongoose.connect('mongodb://localhost/drink_order_system',{useMongoClient:true});
+mongoose.connect('mongodb://localhost:27017/drink_order_system',{useMongoClient:true});
 
 
 app.use(morgan('combined'));   // set the logger
 
 
 app.use(cors());
-app.use(bodyParser.json({type:'*/*'}));
+app.use(express.static(__dirname+'/public'));
+
+// app.use(bodyParser.json({type:'*/*'}));
+
+
 
 
 router(app);
 
 
-app.listen(PORT);
+app.listen(PORT,
+            ()=>console.log(`server is loadup on port:${PORT}`));
 
 
-console.log(`server is loadup on port:${PORT}`);
