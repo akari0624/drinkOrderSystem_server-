4e4403aef3f8a09b48cb9c3b0cf570de21759209
akari0624 <akari0624@hotmail.com>
2018-04-30 00:56:36 +0800

    add new test case

--- test/models/vendor_test.js
+++ test/models/vendor_test.js
@@ -1,51 +1,63 @@
 const assert = require('assert');
 const VendorModel = require('../../models/Vendor');
 
-describe('subdocuments',()=>{
-
- 
-
-    it('can produce sub-document',
-    (done)=>{
-    
-    
-        const chaNoMaTe = new VendorModel({
-    
-            vendor_name:'茶的魔手',
-            vendor_addreass:'高雄市XX區ＯＯ路',
-            vendor_tel:'091122334455',
-            meals:[{name: '紅茶', price: 30 },{name: '綠茶', price: 25 },{name: '烏龍茶', price: 35 }],
-            menuImageString:['/usr/Morris/node/img/XXXX.png','/usr/Morris/node/img/1111.png','/usr/Morris/node/img/2222.png']
-    
-    
-        });
-    
-        chaNoMaTe.save().then(()=>{
-    
-            VendorModel.findOne({vendor_name:'茶的魔手'}).then((tVendor)=>{
-    
-                assert(tVendor.meals[0].name === '紅茶');
-                assert(tVendor.meals[0].price === 30);
-                done();   // handle async operation
+module.exports = () => {
+    describe('sub-documents', () => {
+        it('can produce sub-document', done => {
+            const chaNoMaTe = new VendorModel({
+                vendor_name: '茶的魔手',
+                vendor_addreass: '高雄市XX區ＯＯ路',
+                vendor_tel: '091122334455',
+                meals: [
+                    { name: '紅茶', price: 30 },
+                    { name: '綠茶', price: 25 },
+                    { name: '烏龍茶', price: 35 }
+                ],
+                menuImageString: [
+                    '/usr/Morris/node/img/XXXX.png',
+                    '/usr/Morris/node/img/1111.png',
+                    '/usr/Morris/node/img/2222.png'
+                ]
             });
-        })
-    
-    }
-    );
 
-    it('test find',(done)=>{
- 
-        VendorModel.find('meals').where('price').gte(30).then((meals)=>{
-
-              assert(meals[0].name === '紅茶');
-              assert(meals[0].price === 30);
-              assert(meals.length === 5);
-
-             done();
+            chaNoMaTe.save().then(() => {
+                VendorModel.findOne({ vendor_name: '茶的魔手' }).then(
+                    tVendor => {
+                        assert(tVendor.meals[0].name === '紅茶');
+                        assert(tVendor.meals[0].price === 30);
+                        done(); // handle async operation
+                    }
+                );
+            }).catch(err => {console.log(err); done();});
         });
 
+        it('test find', done => {
+            VendorModel.find('meals')
+                .where('price')
+                .gte(30)
+                .then(meals => {
+                    assert(meals[0].name === '紅茶');
+                    assert(meals[0].price === 30);
+                    assert(meals.length === 5);
+
+                    done();
+                }).catch(err => {console.log(err); done();});
+        });
     });
 
+    describe('select ALL', () => {
+        it('can retrieve all vendors', done => {
+            VendorModel.find()
+                .exec()
+                .then(vendors => {
+                    assert(vendors.length > 0);
 
-});
-
+                    done();
+                })
+                .catch(err => {
+                    console.log(err);
+                    done();
+                });
+        });
+    });
+};
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,18 +1,28 @@
 const mongoose = require('mongoose');
+const vendor_test_suite = require('./models/vendor_test');
 
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/vendor_test',{useMongoClient:true});
+mongoose.connect('mongodb://localhost/vendor_test', { useMongoClient: true });
 
 mongoose.connection
-                .once('open',()=>{console.log('good to go')})
-                .on('error',(errorMsg)=>{console.warn('error occured:',errorMsg)});
+    .once('open', () => {
+        console.log('good to go');
+    })
+    .on('error', errorMsg => {
+        console.warn('error occured:', errorMsg);
+    });
 
+// beforeEach(done => {
+//     mongoose.connection.collections.vendors,
+//     drop(() => {
+//         done();
+//     });
+// });
 
-beforeEach((done)=>{
-
-mongoose.connection.collections.vendors,drop(()=>{
-
-    done();
+describe('start test', () => {
+    it('vendor test suite', done => {
+        vendor_test_suite();
+        done();
+    });
 });
-
-});                
