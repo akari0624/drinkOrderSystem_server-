6ad04326ff721b815d2cbd086cd6d76df2561823
akari0624 <akari0624@hotmail.com>
2018-10-15 15:13:31 +0800

    add passport jwt-auth middleware

---
+++ models/auth/jwt/jwtAuthService.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const PassportJWTStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const UserModel = require('../../users/User');
+const conf = require('../../../conf/key');
+
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader(conf.jwtKeyInEveryRequestHeader),
+    secretOrKey: conf.jwtSecret
+};
+
+// write our jwt strategy after extract jwt String from request
+// header:authorization
+const jwtLogin = new PassportJWTStrategy(jwtOptions, (payload, done) => {
+
+    const userID = payload.sub;
+    UserModel
+        .findOne({userID})
+        .then(user => {
+            if (user) {
+
+                //在這個middleware之後的middleware的req裡加上一個屬性叫user
+                return done(null, user);
+            }
+
+            return done('請勿在未註冊會員的情況下嘗試存取本應用程式的API，謝謝!', false);
+        })
+        .catch(err => {
+            return done(err, false);
+        });
+});
+
+// tell passport to use this strategy
+passport.use(jwtLogin);
+
+
+exports.requireJWTAuth = passport.authenticate('jwt', { session: false });
--- router.js
+++ router.js
@@ -6,25 +6,28 @@ const jsonResponserMiddleware = require('./middleware/jsonResponserFinalMiddlewa
 
 const AuthService = require('./models/auth/facebook/AuthService');
 const passport = require('passport');
+require('./models/auth/facebook/AuthService').applyFaceBookAuthStrategy(passport);
 
+const JWTAuthService = require('./models/auth/jwt/jwtAuthService');
 
 module.exports = function(app) {
     // 在router.js 裡直接套用 multerFileUploadMiddleware , 就是這樣寫
     // VendorDAO 那邊不用管 file, files 怎麼來，用就對了
     // 完全看不到 middleware裡對res做了些什麼,不知道是怎麼做的...非常厲害  = =
-    app.post('/vendor/meal/initlist', UploadImg, VendorService.save_one_Vendor);
 
-    app.post('/vendor/retrieve/:id', VendorService.get_VendorInfo);
+    /* 2018.10.15  在請求這些API之前 要先檢查request是否帶有JWT */
+    app.post('/vendor/meal/initlist', JWTAuthService.requireJWTAuth , UploadImg, VendorService.save_one_Vendor);
 
-    app.post('/order/create', OrderService.Insert_Init_Order, jsonResponserMiddleware);
+    app.post('/vendor/retrieve/:id', JWTAuthService.requireJWTAuth, VendorService.get_VendorInfo);
 
-    app.post('/order/join/get_order_info',OrderService.get_joinOrder_initInfo, jsonResponserMiddleware);
+    app.post('/order/create', JWTAuthService.requireJWTAuth, OrderService.Insert_Init_Order, jsonResponserMiddleware);
 
-    app.post('/order/join/add_order_meal_to_order', OrderService.addMealToOrder, jsonResponserMiddleware);
+    app.post('/order/join/get_order_info', JWTAuthService.requireJWTAuth, OrderService.get_joinOrder_initInfo, jsonResponserMiddleware);
+
+    app.post('/order/join/add_order_meal_to_order', JWTAuthService.requireJWTAuth, OrderService.addMealToOrder, jsonResponserMiddleware);
 
 
     
-    require('./models/auth/facebook/AuthService').applyFaceBookAuthStrategy(passport);
 
 
     app.use(passport.initialize());
