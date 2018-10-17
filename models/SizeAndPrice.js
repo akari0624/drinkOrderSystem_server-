2d690351221439e67210a864f4d0f233e472a39d
akari0624 <akari0624@hotmail.com>
2018-10-17 21:22:23 +0800

    cant use arrow function here

--- models/SizeAndPrice.js
+++ models/SizeAndPrice.js
@@ -2,16 +2,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const SizeAndPrice = new Schema({
+const SizeAndPriceSchema = new Schema({
 
     size:String,
     price:String
 });
 
+                               /*don't use arrow function here! */ 
+SizeAndPriceSchema.pre('save', function(next){
 
-SizeAndPrice.pre('save', next => {
+    console.log('SizeAndPrice', this);
+    const parseResult = parseInt(this.price, 10);
 
-    const parseResult = parseInt(this.price,10);
+    console.log('parse price:',parseResult, this.price);
+    console.log(typeof parseResult);
 
     if(isNaN(parseResult)){
         return next('價格必須為數字型態！');
@@ -24,5 +28,4 @@ SizeAndPrice.pre('save', next => {
 
 
 
-
-module.exports = SizeAndPrice;
+module.exports = SizeAndPriceSchema;
